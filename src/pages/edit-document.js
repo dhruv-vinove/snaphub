d5970623e07d4ab5a90ce8073aee8c25ed323407
dhruv0724 <dhruv.verma@mail.vinove.com>
2024-02-19 15:18:24 +0530

    category page 2

--- src/pages/edit-document.js
+++ src/pages/edit-document.js
@@ -4,13 +4,16 @@ import { subDays, subHours } from "date-fns";
 import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon";
 import ArrowUpOnSquareIcon from "@heroicons/react/24/solid/ArrowUpOnSquareIcon";
 import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
-import { Box, Button, Container, Link, Stack, SvgIcon, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Link, Stack, SvgIcon, TextField, Typography } from "@mui/material";
 import { useSelection } from "src/hooks/use-selection";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { applyPagination } from "src/utils/apply-pagination";
 import { ManageDocumentTable } from "src/sections/managedocument/managedocument-table";
 import { ManageDocumentSearch } from "src/sections/managedocument/managedocument-search";
 import { height } from "@mui/system";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
 const now = new Date();
 
 const data = [
@@ -230,7 +233,7 @@ const Page = () => {
   return (
     <>
       <Head>
-        <title>Manage Document | SnapHub</title>
+        <title>Manage Document | Edit Document | SnapHub</title>
       </Head>
 
       <Box
@@ -244,7 +247,7 @@ const Page = () => {
       >
         <Container maxWidth="xxl">
           <div style={{marginBottom:"22px"}}>
-            Home	&gt; Sig in	&gt; Dashboard &gt; Manage Document &gt; Add New Document
+            Home	&gt; Sig in	&gt; Dashboard &gt; Manage Document &gt; Edit Document
           </div>
 
           <Stack spacing={3}>
@@ -257,7 +260,7 @@ const Page = () => {
                 </svg>
 
                   <Typography variant="h4" sx={{ fontSize: "19px",my:"auto",fontWeight:"400 !important",width:"max-content !important" }}>
-                  Add Document
+                  Edit Document
                   </Typography>
                   </Box>
 
@@ -265,102 +268,67 @@ const Page = () => {
               </Stack>
 
             </Stack>
+            <Container className="px-0" sx={{margin:"28px auto auto auto !important"}}>
+                <Grid container rowSpacing={2} spacing={2} className="card-body impactstoryheader scrollcard-section form">
+                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                            <Typography variant="h6" sx={{ fontSize: "19px",my:"auto",fontWeight:"500 !important",width:"max-content !important" }}>
+                                    Automtaed Folder Pick Up
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Document No.
+                            </label>
+                            <TextField id="outlined-basic" label="" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Document Date
+                            </label>
+                            <TextField type="date" id="outlined-basic" label="" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Amount
+                            </label>
+                            <TextField id="outlined-basic" label="" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Bill to name
+                            </label>
+                            <TextField id="outlined-basic" label="" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Upload document
+                            </label>
+                            <TextField id="outlined-basic" label="" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Description
+                            </label>
+                            <div style={{display:"flex"}}>
+                                {/* <TextField id="outlined-basic" label="" variant="outlined" /> */}
+                                <div className="image-div">
+                                    <img className="profileimg" src="../assets/images/invoice.png"></img>
+                                </div>
+                                <div className='table-action-div' style={{display:"flex",justifyContent:"center" }}>
+                                    <Button className='edit'><FontAwesomeIcon icon={faPencil} /></Button>
+                                    <Button className='delete'><FontAwesomeIcon icon={faTrashCan} /></Button>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12} sx={{display:"grid"}}>
+                            <label style={{textAlign:"start"}}>
+                            Upload document
+                            </label>
+                            <textarea minRows={2} />
+                        </Grid>
+                </Grid>
+            </Container>
 
-            <Stack spacing={3} className="scrollcard-section" sx={{display:"flex",flexDirection:"row"}}>
-                <Box sx={{display:"flex",flexDirection:"row",my:"auto !important",width:{lg:"70%"}}}>
-                    <Box sx={{marginRight:"44px"}}>
-                        <svg width="76" height="85" viewBox="0 0 76 85" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M27.8723 37.0179H60.6495V14.571H46.0735V0H15.3484V27.972H20.8837L27.8723 37.0179ZM58.6216 11.7035L48.942 2.02831V11.7035H58.6216ZM63.5191 37.0179H68.1939C71.1287 37.0179 73.8018 38.2196 75.7342 40.1513L76 40.4265V29.6242C76 27.4814 75.1195 25.5277 73.7052 24.1149C72.2919 22.7011 70.3385 21.8209 68.1939 21.8209H63.5191V37.0179ZM12.4798 21.8209H7.80605C5.66146 21.8209 3.70811 22.7011 2.2938 24.1149C1.25355 25.1548 0.50226 26.4867 0.177577 27.972H12.4798V21.8209ZM68.1939 39.8855H26.4759L19.4852 30.8395H0V43.9778V76.6839C0 78.8278 0.879481 80.7815 2.2938 82.1953C3.70811 83.6081 5.66146 84.4883 7.80605 84.4883H68.1939C70.3385 84.4883 72.2919 83.6081 73.7052 82.1953C75.1195 80.7815 76 78.8278 76 76.6839V47.6889C76 45.5461 75.1195 43.5923 73.7052 42.1796C72.2919 40.7657 70.3385 39.8855 68.1939 39.8855Z" fill="#BABEC9"/>
-                        </svg>
-                    </Box>
-                    <Box sx={{gap:"10px",display:"flex",flexDirection:"column"}}>
-                        <Typography variant="h4" sx={{ fontSize: "19px",my:"auto",fontWeight:"500 !important",width:"max-content !important" }}>
-                            Automtaed Folder Pick Up
-                        </Typography>
-                        <Typography sx={{ fontSize: "15px",my:"auto",fontWeight:"300 !important",color:"#191F2F" }}>
-                            <svg style={{marginRight:"10px"}} width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5 0C3.67392 0 2.40215 0.526784 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C2.40215 9.47322 3.67392 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 3.67392 9.47322 2.40215 8.53553 1.46447C7.59785 0.526784 6.32608 0 5 0ZM2.5 7.5L2.61 7.242C2.92467 6.50867 3.24033 5.77333 3.557 5.036C3.56291 5.02403 3.56598 5.01085 3.56598 4.9975C3.56598 4.98415 3.56291 4.97097 3.557 4.959C3.21033 4.15433 2.864 3.349 2.518 2.543L2.5 2.5L7.5 5L2.5 7.5Z" fill="#2C2C2C"/>
-                            </svg>
-                            Designated specific folder where document are stored.
-                        </Typography>
-                        <Typography sx={{ fontSize: "15px",my:"auto",fontWeight:"300 !important",color:"#191F2F" }}>
-                            <svg style={{marginRight:"10px"}} width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M5 0C3.67392 0 2.40215 0.526784 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C2.40215 9.47322 3.67392 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 3.67392 9.47322 2.40215 8.53553 1.46447C7.59785 0.526784 6.32608 0 5 0ZM2.5 7.5L2.61 7.242C2.92467 6.50867 3.24033 5.77333 3.557 5.036C3.56291 5.02403 3.56598 5.01085 3.56598 4.9975C3.56598 4.98415 3.56291 4.97097 3.557 4.959C3.21033 4.15433 2.864 3.349 2.518 2.543L2.5 2.5L7.5 5L2.5 7.5Z" fill="#2C2C2C"/>
-                            </svg>
-                            Application periodicaaly scan these folder automatically extract and process documents.
-                        </Typography>
-                        <Button
-                        className="btn-gray"
-                        color="inherit"
-                        >
-                            Select Folder to Pick up Document
-                    </Button>
-                    </Box>
-                </Box>
-                <Box sx={{width:{lg:"30%",gap:"10px",flexDirection:"column",display:"flex"}}}>
-                    <Box sx={{background:"#f5f7ff",px:"24px",py:"27px",borderRadius:"8px"}}>
-                        <Box sx={{display:"flex",flexDirection:"row"}}>
-                            <Box>
-                            <svg width="41" height="52" viewBox="0 0 41 52" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M20.4999 11.9153C20.5942 11.9153 20.6884 11.9362 20.7752 11.9777L23.3832 13.2238V5.76855H17.6167V13.2238L20.2247 11.9777C20.3115 11.9362 20.4057 11.9153 20.4999 11.9153Z" fill="#BBC0CE"/>
-                            <path d="M13.2522 20.4262H27.7476C28.3312 20.4262 28.8057 19.955 28.8057 19.3754V6.81928C28.8057 6.23974 28.3312 5.76855 27.7476 5.76855H24.6528V14.2269C24.6528 14.4436 24.5404 14.6451 24.3561 14.7605C24.2527 14.8249 24.1353 14.8573 24.018 14.8573C23.9237 14.8573 23.8303 14.8368 23.7427 14.7949L20.4999 13.2455L17.2571 14.7949C17.0604 14.8885 16.8306 14.8766 16.6438 14.7605C16.4594 14.6451 16.347 14.4436 16.347 14.2269V5.76855H13.2522C12.6686 5.76855 12.1941 6.23974 12.1941 6.81928V19.3754C12.1941 19.955 12.6686 20.4262 13.2522 20.4262Z" fill="#BBC0CE"/>
-                            <path d="M36.9794 0.304199H4.02065C1.80367 0.304199 0 2.09536 0 4.29695V48.0071C0 48.2156 0.104156 48.4106 0.276917 48.5279L5.24155 51.8903C5.45812 52.0364 5.74082 52.0364 5.95739 51.8903L10.5633 48.7701L15.17 51.8903C15.3866 52.0364 15.6693 52.0364 15.8858 51.8903L20.4909 48.7701L25.0968 51.8903C25.2051 51.9633 25.3299 51.9999 25.4547 51.9999C25.5795 51.9999 25.7043 51.9633 25.8126 51.8903L30.4227 48.7701L35.0368 51.8907C35.2517 52.036 35.5361 52.036 35.751 51.8907L40.7223 48.5284C40.8958 48.4106 41 48.2156 41 48.0071V4.29695C41 2.09536 39.1963 0.304199 36.9794 0.304199ZM10.9245 6.81869C10.9245 5.54387 11.9685 4.5071 13.2523 4.5071H27.7477C29.0315 4.5071 30.0755 5.54387 30.0755 6.81869V19.3749C30.0755 20.6497 29.0315 21.6864 27.7477 21.6864H13.2523C11.9685 21.6864 10.9245 20.6497 10.9245 19.3749V6.81869ZM32.6677 40.1267H8.33226C7.98178 40.1267 7.69742 39.8443 7.69742 39.4962C7.69742 39.1482 7.98178 38.8658 8.33226 38.8658H32.6677C33.0182 38.8658 33.3026 39.1482 33.3026 39.4962C33.3026 39.8443 33.0182 40.1267 32.6677 40.1267ZM36.1329 35.7924H4.8671C4.51662 35.7924 4.23226 35.51 4.23226 35.162C4.23226 34.8139 4.51662 34.5316 4.8671 34.5316H36.1329C36.4834 34.5316 36.7677 34.8139 36.7677 35.162C36.7677 35.51 36.4834 35.7924 36.1329 35.7924ZM36.7942 30.8277C36.7942 31.1758 36.5098 31.4582 36.1594 31.4582H5.21096C4.86049 31.4582 4.57612 31.1758 4.57612 30.8277V26.152C4.57612 25.804 4.86049 25.5216 5.21096 25.5216H36.1594C36.5098 25.5216 36.7942 25.804 36.7942 26.152V30.8277Z" fill="#BBC0CE"/>
-                            <path d="M35.5249 26.7827H5.84619V30.1976H35.5249V26.7827Z" fill="#BBC0CE"/>
-                            </svg>
-                            </Box>
-                            <Box sx={{flexDirection:"row",display:"flex"}}>
-                                <Typography sx={{my:"auto",mx:"10px"}} >Add document<br>
-                                </br> manually</Typography>
-                            </Box>
-                        </Box>
-                                {/* <Button
-                                sx={{width:"100%"}}
-                                    className="btn-white"
-                                    color="inherit"
-                                    >
-                                        Manual Upload
-                                </Button> */}
-                                <Button
-                                  sx={{width:"100%"}}
-                                  className="btn-white"
-                                  color="inherit"
-                                  href="/edit-document"
-                                    // startIcon={(
-                                    //   <SvgIcon fontSize="small">
-                                    //     <ArrowUpOnSquareIcon />
-                                    //   </SvgIcon>
-                                    // )}
-                                  >
-                                    Add document manually
-                                </Button>
-                        <Box></Box>
-                    </Box>
-                    <Box sx={{background:"#f5f7ff",px:"24px",py:"27px",borderRadius:"8px"}}>
-                        <Box sx={{display:"flex",flexDirection:"row"}}>
-                            <Box>
-                            <svg width="41" height="52" viewBox="0 0 41 52" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M20.4999 11.9153C20.5942 11.9153 20.6884 11.9362 20.7752 11.9777L23.3832 13.2238V5.76855H17.6167V13.2238L20.2247 11.9777C20.3115 11.9362 20.4057 11.9153 20.4999 11.9153Z" fill="#BBC0CE"/>
-                            <path d="M13.2522 20.4262H27.7476C28.3312 20.4262 28.8057 19.955 28.8057 19.3754V6.81928C28.8057 6.23974 28.3312 5.76855 27.7476 5.76855H24.6528V14.2269C24.6528 14.4436 24.5404 14.6451 24.3561 14.7605C24.2527 14.8249 24.1353 14.8573 24.018 14.8573C23.9237 14.8573 23.8303 14.8368 23.7427 14.7949L20.4999 13.2455L17.2571 14.7949C17.0604 14.8885 16.8306 14.8766 16.6438 14.7605C16.4594 14.6451 16.347 14.4436 16.347 14.2269V5.76855H13.2522C12.6686 5.76855 12.1941 6.23974 12.1941 6.81928V19.3754C12.1941 19.955 12.6686 20.4262 13.2522 20.4262Z" fill="#BBC0CE"/>
-                            <path d="M36.9794 0.304199H4.02065C1.80367 0.304199 0 2.09536 0 4.29695V48.0071C0 48.2156 0.104156 48.4106 0.276917 48.5279L5.24155 51.8903C5.45812 52.0364 5.74082 52.0364 5.95739 51.8903L10.5633 48.7701L15.17 51.8903C15.3866 52.0364 15.6693 52.0364 15.8858 51.8903L20.4909 48.7701L25.0968 51.8903C25.2051 51.9633 25.3299 51.9999 25.4547 51.9999C25.5795 51.9999 25.7043 51.9633 25.8126 51.8903L30.4227 48.7701L35.0368 51.8907C35.2517 52.036 35.5361 52.036 35.751 51.8907L40.7223 48.5284C40.8958 48.4106 41 48.2156 41 48.0071V4.29695C41 2.09536 39.1963 0.304199 36.9794 0.304199ZM10.9245 6.81869C10.9245 5.54387 11.9685 4.5071 13.2523 4.5071H27.7477C29.0315 4.5071 30.0755 5.54387 30.0755 6.81869V19.3749C30.0755 20.6497 29.0315 21.6864 27.7477 21.6864H13.2523C11.9685 21.6864 10.9245 20.6497 10.9245 19.3749V6.81869ZM32.6677 40.1267H8.33226C7.98178 40.1267 7.69742 39.8443 7.69742 39.4962C7.69742 39.1482 7.98178 38.8658 8.33226 38.8658H32.6677C33.0182 38.8658 33.3026 39.1482 33.3026 39.4962C33.3026 39.8443 33.0182 40.1267 32.6677 40.1267ZM36.1329 35.7924H4.8671C4.51662 35.7924 4.23226 35.51 4.23226 35.162C4.23226 34.8139 4.51662 34.5316 4.8671 34.5316H36.1329C36.4834 34.5316 36.7677 34.8139 36.7677 35.162C36.7677 35.51 36.4834 35.7924 36.1329 35.7924ZM36.7942 30.8277C36.7942 31.1758 36.5098 31.4582 36.1594 31.4582H5.21096C4.86049 31.4582 4.57612 31.1758 4.57612 30.8277V26.152C4.57612 25.804 4.86049 25.5216 5.21096 25.5216H36.1594C36.5098 25.5216 36.7942 25.804 36.7942 26.152V30.8277Z" fill="#BBC0CE"/>
-                            <path d="M35.5249 26.7827H5.84619V30.1976H35.5249V26.7827Z" fill="#BBC0CE"/>
-                            </svg>
-                            </Box>
-                            <Box sx={{flexDirection:"row",display:"flex"}}>
-                                <Typography sx={{my:"auto",mx:"10px"}}>Scan and upload<br></br>document</Typography>
-                            </Box>
-                        </Box>
-                                <Button
-                                sx={{width:"100%"}}
-
-                                    className="btn-white"
-                                    color="inherit"
-                                    >
-                                        Scan document
-                                </Button>
-                        <Box></Box>
-                    </Box>
-                </Box>
-            </Stack>
           </Stack>
         </Container>
       </Box>
